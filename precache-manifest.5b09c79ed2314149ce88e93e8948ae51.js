9354bf59eb5da1fdd456edc6472cb1ee64f223dc
Kapoor <akshkapoor@deloitte.com>
2020-12-14 16:28:39 +0530

    Updates

---
+++ precache-manifest.5b09c79ed2314149ce88e93e8948ae51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec34e9aa5fb467b5e76cc4d1973b02e",
+    "url": "/RoboFriendsApp/index.html"
+  },
+  {
+    "revision": "ed9b6b7b3f5e1db31da5",
+    "url": "/RoboFriendsApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "66688f3f818d91049e08",
+    "url": "/RoboFriendsApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ed9b6b7b3f5e1db31da5",
+    "url": "/RoboFriendsApp/static/js/2.12e19136.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriendsApp/static/js/2.12e19136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66688f3f818d91049e08",
+    "url": "/RoboFriendsApp/static/js/main.97ea0731.chunk.js"
+  },
+  {
+    "revision": "bcddb2eabc257623e144",
+    "url": "/RoboFriendsApp/static/js/runtime-main.c6b5270a.js"
+  }
+]);
